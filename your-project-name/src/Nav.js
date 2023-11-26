86fd551ede627f9792eb5d14edf2b21981227019
Laikemariam <112988201+laikemariam1221@users.noreply.github.com>
2023-11-26 07:39:49 +0300

    Initial commit

---
+++ your-project-name/src/Nav.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+import image from './a.jpg';
+
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+      src={image}
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
+        alt="Avatar logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
